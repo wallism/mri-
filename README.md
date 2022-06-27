3714529dbd75111fcc8b936b9e7b065ac42b1a8e
wallism <mark.wallis@mrisoftware.com>
2022-06-27 23:00:54 +1000

    initial running build

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # samples showing how to use DAPR with docker compose.
+
+Quick, clear, useful intro for .net devs
+https://docs.microsoft.com/en-us/dotnet/architecture/dapr-for-net-developers/getting-started
+
+
+
+Running locally info
+https://markheath.net/post/running-locally-with-dapr-options
---
+++ public-api/public-api/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace public_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet("ping")]
+        public string Get()
+        {
+            return "pong";
+        }
+
+        [HttpGet("check")]
+        public IActionResult GetCheck()
+        {
+            return Ok("all good");
+        }
+
+        
+    }
+}
