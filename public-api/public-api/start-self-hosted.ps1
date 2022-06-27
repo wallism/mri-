b5a365473cb0541f148a559bf94777161e398738
wallism <mark.wallis@mrisoftware.com>
2022-06-28 09:06:31 +1000

    move to src

--- public-api/public-api/start-self-hosted.ps1
+++
@@ -1,7 +0,0 @@
-# just an example of an alternative way to start, now using compose
-dapr run `
- --app-id public-api `
- --app-port 5200 `
- --dapr-http-port 3501 `
- --components-path ../../dapr/components `
- dotnet run 
